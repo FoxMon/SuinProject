778afe102c77fb62e3a320b0f63e20b1f3c886a2
FoxMon <jjockrod@naver.com>
2021-05-05 13:55:07 +0900

    get_post_api

--- suinbook/src/main/java/suinshop/suinbook/api/MemberApiController.java
+++ suinbook/src/main/java/suinshop/suinbook/api/MemberApiController.java
@@ -2,14 +2,31 @@ package suinshop.suinbook.api;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import suinshop.suinbook.domain.Member;
 import suinshop.suinbook.service.MemberService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+
+        return memberService.findMembers();
+    }
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody Member member) {
+
+        Long id = memberService.join(member);
+
+        return new CreateMemberResponse(id);
+    }
 
     @Data
     static class CreateMemberRequest {
